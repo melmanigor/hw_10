6572794c8c54128203f81497cc1127de393bc158
melmanigor <84411187+melmanigor@users.noreply.github.com>
2024-12-15 22:11:28 +0200

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,19 @@
+
+# def add_three_numbers(a, b, c):
+# 	print(a)
+# 	print(b)
+# 	print(c)
+# 	return a + b + c
+
+def return_two_values():
+	print(1)
+	print(2)
+	return 1, 2
+
+result = return_two_values()
+print(type(result))
+
+x, y = result
+print(x)
+print(y)
+
